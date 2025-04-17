d06b2ecb9f9aeba5bab629639648ddb16f408f07
yjgaia <yjdev88@gmail.com>
2025-04-17 17:28:11 +0900

    Generate metadata files for Doge Sound Club collections
    - Implemented a script to generate metadata for biased mates, e-mates, and mates collections.
    - Created JSON files for each collection with structured metadata including id, name, image, thumbnail, description, external URL, and attributes.
    - The generated metadata files are located in their respective directories: dogesoundclub-biased-mates, dogesoundclub-e-mates, and dogesoundclub-mates.

---
+++ utils/generate-metadata.ts
@@ -0,0 +1,76 @@
+import biasedMatesMetadataLegacy from "../dogesoundclub-biased-mates/metadata-legacy.json" with {
+  type: "json",
+};
+import eMatesMetadataLegacy from "../dogesoundclub-e-mates/metadata-legacy.json" with {
+  type: "json",
+};
+import matesMetadataLegacy from "../dogesoundclub-mates/metadata-legacy.json" with {
+  type: "json",
+};
+import fs from "fs";
+
+const biasedMatesMetadata: any[] = [];
+for (const metadata of biasedMatesMetadataLegacy as any) {
+  const type = metadata.type ? metadata.type : "bmcs";
+
+  biasedMatesMetadata.push({
+    id: metadata.id,
+    name: metadata.name,
+    image:
+      `https://matedevdao.github.io/static-kaia-nft-assets/dogesoundclub-biased-mates/images/${type}/${metadata.id}.png`,
+    thumbnail:
+      `https://matedevdao.github.io/static-kaia-nft-assets/dogesoundclub-biased-mates/thumbnails/${type}/${metadata.id}.png`,
+    description: metadata.description,
+    external_url: metadata.external_url,
+    attributes: metadata.attributes,
+  });
+}
+
+fs.writeFileSync(
+  "../dogesoundclub-biased-mates/metadata.json",
+  JSON.stringify(biasedMatesMetadata, null, 2),
+);
+
+const eMatesMetadata: any[] = [];
+for (const metadata of eMatesMetadataLegacy as any) {
+  eMatesMetadata.push({
+    id: metadata.tokenId,
+    name: metadata.name,
+    image:
+      `https://matedevdao.github.io/static-kaia-nft-assets/dogesoundclub-e-mates/images/${metadata.tokenId}.png`,
+    thumbnail:
+      `https://matedevdao.github.io/static-kaia-nft-assets/dogesoundclub-e-mates/thumbnails/${metadata.tokenId}.png`,
+    description: metadata.description,
+    external_url: "https://dogesound.club/",
+    attributes: metadata.attributes,
+  });
+}
+
+fs.writeFileSync(
+  "../dogesoundclub-e-mates/metadata.json",
+  JSON.stringify(eMatesMetadata, null, 2),
+);
+
+const matesMetadata: any[] = [];
+for (const metadata of matesMetadataLegacy as any) {
+  const id = parseInt(metadata.name.split("#")[1]);
+
+  matesMetadata.push({
+    id,
+    name: metadata.name,
+    image:
+      `https://matedevdao.github.io/static-kaia-nft-assets/dogesoundclub-mates/images/${id}.png`,
+    thumbnail:
+      `https://matedevdao.github.io/static-kaia-nft-assets/dogesoundclub-mates/thumbnails/${id}.png`,
+    description: metadata.description,
+    external_url: "https://dogesound.club/",
+    attributes: metadata.attributes,
+  });
+}
+
+fs.writeFileSync(
+  "../dogesoundclub-mates/metadata.json",
+  JSON.stringify(matesMetadata, null, 2),
+);
+
+console.log("Metadata generated successfully");
